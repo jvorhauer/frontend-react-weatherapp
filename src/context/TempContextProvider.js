3ac9c6695a71dd7a7d89de8c156b5e572d3d91fa
Nova Eeken <novaeeken@Novas-MacBook-Pro.local>
2020-12-30 16:02:36 +0100

    Add logic functions to context

--- src/context/TempContextProvider.js
+++ src/context/TempContextProvider.js
@@ -1,9 +1,25 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+import kelvinToCelcius from '../helpers/kelvinToCelcius';
+import kelvinToFahrenheit from '../helpers/kelvinToFahrenheit';
+
 export const TempContext = createContext(null);
 
 function TempContextProvider({ children }) {
+  const [selectedMetric, toggleSelectedMetric] = useState('celcius');
+
+  function toggleTemp() {
+    if (selectedMetric === 'celcius') {
+      toggleSelectedMetric('fahrenheit');
+    } else {
+      toggleSelectedMetric('celcius');
+    }
+  }
+
   return (
-    <TempContext.Provider value={}>
+    <TempContext.Provider value={{
+      toggleTemp: toggleTemp,
+      kelvinToMetric: selectedMetric === 'celcius' ? kelvinToCelcius : kelvinToFahrenheit,
+    }}>
       { children }
     </TempContext.Provider>
   )
